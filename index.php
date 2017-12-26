4442b949f50cbfa1153b13deee7b9322d5148f03
root <imzoughene@outlook.com>
2017-12-26 11:45:57 +0000

    Creating Single Item

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<title>Online Shopping</title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/style.css" >
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+	          <div class="container-fluid">
+			<div class="navbar-header">
+				<a href="" class="navbar-brand">Online Shopping 
+				</a>	
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="#">Home</a></li>	
+				<li><a href="#">Men</a></li>
+				<li><a href="#">Women</a></li>
+				<li><a href="#">Kids</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">LogOut</a></li>
+			</ul>
+		   </div>
+		</nav>
+		<div class="container">
+	            <div class="row">
+			<div class="col-md-3">
+			<div class="col-md-12 single-item noPadding">
+				<div class="top"></div>
+				<div class="bottom">
+				<h3 class="item-title">Beautifull Watch
+				</h3>	
+				<div class="pull-right cutted-price text-muted"><del>$ 500/=
+				</del></div>
+				<div class="clearfix"></div>
+				<div class="pull-right discounted-price">$ 450/=
+				</div>			
+				</div>
+			</div></div>
+			<!--<div class="col-md-3"><div class="col-md-12 single-item">
+			</div>
+			<div class="col-md-3"><div class="col-md-12 single-item">
+			</div>
+			<div class="col-md-3"><div class="col-md-12 single-item">
+			</div>-->
+	            </div>
+		</div>
+	 
+	</body>
+</html>
