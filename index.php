ceba02676663b226957af919539c8195d3028339
root <imzoughene@outlook.com>
2018-01-07 14:58:46 +0000

    update

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+include "connection.php";
+?>
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Online Shopping</title>
@@ -11,115 +15,35 @@
 		<?php include "includes/header.php"; ?>
 		<div class="container">
 	            <div class="row">
+		        <?php
+			$sql = "select * from items";
+			$run = mysqli_query($conn,$sql);
+			while($rows = mysqli_fetch_assoc($run)){
+			 ?>
 			<div class="col-md-3">
 			<div class="col-md-12 single-item noPadding">
 				<div class="top">
-				<img src="images/Apple_Watch.jpg">
+				<img src="<?php echo $rows["item_image"]; ?>">
 							
 				</div>
 				<div class="bottom">
 				<h3 class="item-title">
-				<a href="product.php">				
-				Beautifull Watch
+				<a 
+		     href="item.php?item_title=<?php echo str_replace(' ','-',$rows["item_title"]);?>&item_id=<?php echo $rows["item_id"];?>">				
+				<?php echo $rows["item_title"]; ?>
 				</a>
 				</h3>	
-				<div class="pull-right cutted-price text-muted"><del>$ 500/=
+				<div class="pull-right cutted-price text-muted"><del>$ 
+				<?php echo $rows["item_price"]; ?>/=
 				</del></div>
 				<div class="clearfix"></div>
-				<div class="pull-right discounted-price">$ 450/=
+				<div class="pull-right discounted-price">$
+				<?php echo $rows["item_price"]-$rows["item_discount"]; ?>/=
 				</div>			
 				</div>
 			</div></div>
-			<div class="col-md-3">
-			<div class="col-md-12 single-item noPadding">
-				<div class="top">
-				<img src="images/Apple_Watch.jpg">
-				</div>
-				<div class="bottom">
-				<h3 class="item-title">Beautifull Watch
-				</h3>	
-				<div class="pull-right cutted-price text-muted"><del>$ 500/=
-				</del></div>
-				<div class="clearfix"></div>
-				<div class="pull-right discounted-price">$ 450/=
-				</div>			
-				</div>
-			</div></div>
-			<div class="col-md-3">
-			<div class="col-md-12 single-item noPadding">
-				<div class="top">
-				<img src="images/Apple_Watch.jpg">				
-				</div>
-				<div class="bottom">
-				<h3 class="item-title">Beautifull Watch
-				</h3>	
-				<div class="pull-right cutted-price text-muted"><del>$ 500/=
-				</del></div>
-				<div class="clearfix"></div>
-				<div class="pull-right discounted-price">$ 450/=
-				</div>			
-				</div>
-			</div></div>
-			<div class="col-md-3">
-			<div class="col-md-12 single-item noPadding">
-				<div class="top">
-				<img src="images/Apple_Watch.jpg">
-				</div>
-				<div class="bottom">
-				<h3 class="item-title">Beautifull Watch
-				</h3>	
-				<div class="pull-right cutted-price text-muted"><del>$ 500/=
-				</del></div>
-				<div class="clearfix"></div>
-				<div class="pull-right discounted-price">$ 450/=
-				</div>			
-				</div>
-			</div></div>
-			<div class="col-md-3">
-			<div class="col-md-12 single-item noPadding">
-				<div class="top">
-				<img src="images/Apple_Watch.jpg">
-				</div>
-				<div class="bottom">
-				<h3 class="item-title">Beautifull Watch
-				</h3>	
-				<div class="pull-right cutted-price text-muted"><del>$ 500/=
-				</del></div>
-				<div class="clearfix"></div>
-				<div class="pull-right discounted-price">$ 450/=
-				</div>			
-				</div>
-			</div></div>
-			<div class="col-md-3">
-			<div class="col-md-12 single-item noPadding">
-				<div class="top">
-				<img src="images/Apple_Watch.jpg">				
-				</div>
-				<div class="bottom">
-				<h3 class="item-title">Beautifull Watch
-				</h3>	
-				<div class="pull-right cutted-price text-muted"><del>$ 500/=
-				</del></div>
-				<div class="clearfix"></div>
-				<div class="pull-right discounted-price">$ 450/=
-				</div>			
-				</div>
-			</div></div>
-			<div class="col-md-3">
-			<div class="col-md-12 single-item noPadding">
-				<div class="top">
-				<img src="images/Apple_Watch.jpg">
-				</div>
-				<div class="bottom">
-				<h3 class="item-title">Beautifull Watch
-				</h3>	
-				<div class="pull-right cutted-price text-muted"><del>$ 500/=
-				</del></div>
-				<div class="clearfix"></div>
-				<div class="pull-right discounted-price">$ 450/=
-				</div>			
-				</div>
-			</div></div>
+			<?php } ?>
+			
 			
 	            </div>
 		</div><div class="clearfix"></div>
