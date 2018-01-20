9aeb8eac365c2f31982d3fa90a08af1b7d17eed3
root <imzoughene@outlook.com>
2018-01-20 16:43:48 +0000

    updating

--- buy.php
+++ buy.php
@@ -108,24 +108,29 @@ if(isset($_GET['check_item_id'])){
 				   </thead>
 				   <tbody>
                     <?php 
-                    $check_select_query ="select * from checkout
+                    $c = 1 ;
+                    $check_select_query ="select * from checkout c
+                    join items i on c.check_item = i.item_id
                     where check_ref = '$_SESSION[ref]'";
                     $check_sel_run = mysqli_query($conn,$check_select_query);
                     while($reader_check = mysqli_fetch_assoc($check_sel_run)){
+                    $new_price = $reader_check['item_price'] - $reader_check['item_discount'];
                     ?>
 					<tr>
-					   <td>1</td>
-					   <td>Beautiful watch</td>
+					   <td><?php echo $c; ?></td>
+					   <td><?php echo $reader_check['item_title']; ?></td>
 				           <td>1</td>
 					   <td>
 					   <button class="btn btn-danger btn-sm">Delete
 					   </button></td>
-					   <td class="text-right"><b>100/=</b></td>
+					   <td class="text-right"><b><?php echo $new_price; ?>/=</b></td>
 					   
 				           <td class="text-right"><b>100/=</b></td>
 					   
 					</tr>
-					<?php } ?>
+					<?php 
+                    $c++;
+                    } ?>
 				   </tbody>
 				</table>
 				<table class="table">
